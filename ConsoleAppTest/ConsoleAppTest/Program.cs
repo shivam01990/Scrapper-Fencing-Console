e1522682504a05b70adb1f5eb6331bf862f9e7b8
Shivam Srivastava <shivam01990@gmail.com>
2016-08-16 18:18:24 +0530

    Add excel library

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -1,6 +1,10 @@
 ﻿using HtmlAgilityPack;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +30,9 @@ namespace ConsoleAppTest
 
                 HtmlDocument doc = new HtmlDocument();
                 doc.LoadHtml(outhtml);
+
                 //HtmlNode divState = doc.DocumentNode.SelectNodes("//div[contains(@class, 'well') and contains(@class, 'well-sm')]//div[@class='row phn']")[1];
+
                 HtmlNodeCollection anchorStates = doc.DocumentNode.SelectNodes("(//div[contains(@class, 'well') and contains(@class, 'well-sm')]//div[@class='row phn'])[2]//ul//li//a");
 
                 List<StateList> _statelist = new List<StateList>();
@@ -40,6 +46,48 @@ namespace ConsoleAppTest
                         _statelist.Add(temp);
                     }
                 }
+
+                string LogPath = AppDomain.CurrentDomain.BaseDirectory;
+                string filename = "\\Finance.xlsx";
+                string filepath = LogPath + filename;
+                var file = new FileInfo(filepath);
+                using (var package = new ExcelPackage(file))
+                {
+                    int RowNum = 1;
+                    int ColNum = 1;
+                    ExcelWorksheet ws = package.Workbook.Worksheets.Add("Finance - " + DateTime.Now.ToShortDateString());
+                    //Merging cells and create a center heading for out table
+                    ws.Cells[RowNum, ColNum].Value = "Finance Companies"; // Heading Name
+                    RowNum++;
+
+
+                    ws.Cells[RowNum, ColNum].Value = "Comany Name"; // Heading Name
+                    ws.Cells[RowNum, ColNum, RowNum + 1, ColNum].Merge = true; //Merge columns start and end range
+                    ws.Cells[RowNum, ColNum, RowNum + 1, ColNum].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    ws.Cells[RowNum, ColNum, RowNum + 1, ColNum].Style.Fill.BackgroundColor.SetColor(Color.Black);
+                    ws.Cells[RowNum, ColNum, RowNum + 1, ColNum].Style.Font.Bold = true; //Font should be bold    
+                    ws.Cells[RowNum, ColNum, RowNum + 1, ColNum].Style.Font.Color.SetColor(Color.White);
+                    ws.Cells[RowNum, ColNum, RowNum + 1, ColNum].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center; // Aligmnet is center
+
+                    ws.Cells[RowNum, ColNum + 1].Value = "Email"; // Heading Name
+                    ws.Cells[RowNum, ColNum + 1, RowNum + 1, ColNum + 1].Merge = true; //Merge columns start and end range
+                    ws.Cells[RowNum, ColNum + 1, RowNum + 1, ColNum + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    ws.Cells[RowNum, ColNum + 1, RowNum + 1, ColNum + 1].Style.Fill.BackgroundColor.SetColor(Color.Black);
+                    ws.Cells[RowNum, ColNum + 1, RowNum + 1, ColNum + 1].Style.Font.Bold = true; //Font should be bold    
+                    ws.Cells[RowNum, ColNum + 1, RowNum + 1, ColNum + 1].Style.Font.Color.SetColor(Color.White);
+                    ws.Cells[RowNum, ColNum + 1, RowNum + 1, ColNum + 1].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center; // Aligmnet is center
+                    RowNum++;
+                    package.Save();
+                    //foreach (StateList state in _statelist)
+                    //{
+                    //    string tempurl = "http://www.manta.com"+state.StateUrl;
+                    //    StateProvider.StartScrap(tempurl, ie, ws, RowNum, ColNum);
+                    //    RowNum++;
+                    //}
+                    // save our new workbook and we are done!
+                    package.Save();
+                }
+
 
                 // Close Internet Explorer and the console window immediately.
                 ie.Close();
--- ConsoleAppTest/ConsoleAppTest/StateProvider.cs
+++ ConsoleAppTest/ConsoleAppTest/StateProvider.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +12,17 @@ namespace ConsoleAppTest
 {
     public class StateProvider
     {
-        public static void StartScrap(string scrapUrl, IE ie)
+        public static void StartScrap(string scrapUrl, IE ie, ExcelWorksheet ws, int RowNum, int ColNum)
         {
             ie.GoTo(scrapUrl);
+            System.Threading.Thread.Sleep(5000);
+            HtmlDocument doc = new HtmlDocument();
+            string outhtml = ie.Html;
+            doc.LoadHtml(outhtml);
+
+            ws.Cells[RowNum, ColNum].Value = scrapUrl;
+       
+
         }
     }
 }
