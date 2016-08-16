395c07a07b0d13363da66b87274f2d75cded67d9
Shivam Srivastava <shivam01990@gmail.com>
2016-08-16 17:10:04 +0530

    Scrapper

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -23,29 +23,42 @@ namespace ConsoleAppTest
                 Console.WriteLine(ie.Html);
 
                 string outhtml = ie.Html;
-                
+
                 HtmlDocument doc = new HtmlDocument();
                 doc.LoadHtml(outhtml);
                 //HtmlNode divState = doc.DocumentNode.SelectNodes("//div[contains(@class, 'well') and contains(@class, 'well-sm')]//div[@class='row phn']")[1];
                 HtmlNodeCollection anchorStates = doc.DocumentNode.SelectNodes("(//div[contains(@class, 'well') and contains(@class, 'well-sm')]//div[@class='row phn'])[2]//ul//li//a");
+
+                List<StateList> _statelist = new List<StateList>();
                 if (anchorStates != null)
                 {
                     foreach (var link in anchorStates)
                     {
-                        string hrefValue = link.Attributes["href"].Value;
-                        string textValue = link.InnerText;
+                        StateList temp = new StateList();
+                        temp.StateUrl = link.Attributes["href"].Value;
+                        temp.StateName = link.InnerText;
+                        _statelist.Add(temp);
                     }
                 }
-                
+
                 // Close Internet Explorer and the console window immediately.
                 ie.Close();
             }
 
-           
-           
 
-       
+
+
+
             Console.ReadKey();
         }
+
+
+    }
+
+
+    public class StateList
+    {
+        public string StateName { get; set; }
+        public string StateUrl { get; set; }
     }
 }
---
+++ ConsoleAppTest/ConsoleAppTest/StateProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatiN.Core;
+
+namespace ConsoleAppTest
+{
+    public class StateProvider
+    {
+        public static void StartScrap(string scrapUrl, IE ie)
+        {
+            ie.GoTo(scrapUrl);
+        }
+    }
+}
